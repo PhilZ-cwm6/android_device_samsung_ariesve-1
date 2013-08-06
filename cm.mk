a84d8016b66f3d19d5126dc295efd3495ada9fd4
david <davidteri91@gmail.com>
2013-08-06 16:35:40 +0200

    Android 4.3

---
+++ cm.mk
@@ -0,0 +1,21 @@
+# Specify phone tech before including full_phone
+$(call inherit-product, vendor/cm/config/gsm.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, $(LOCAL_PATH)/full_ariesve.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := ariesve
+
+# Setup device configuration
+PRODUCT_NAME := cm_ariesve
+PRODUCT_DEVICE := ariesve
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_MODEL := GT-I9001
+
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=samsung/GT-I9001/GT-I9001:2.3.6/GINGERBREAD/XXKQN:user/release-keys PRIVATE_BUILD_DESC="GT-I9001-user 2.3.6 GINGERBREAD XXKQN release-keys"
+
