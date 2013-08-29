ccc114d672a8ae9d45ca45e9e73a1b2154ddf048
david <davidteri91@gmail.com>
2013-08-29 23:25:09 +0200

    Add flags for twrp 2.6.0.4

--- ariesve.mk
+++ ariesve.mk
@@ -52,6 +52,7 @@ PRODUCT_COPY_FILES += \
 # Recovery stuff
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/ramdisk/fstab.qcom:recovery/root/fstab.qcom \
+    $(LOCAL_PATH)/recovery/twrp.fstab:recovery/root/etc/twrp.fstab \
     $(LOCAL_PATH)/recovery/postrecoveryboot.sh:recovery/root/sbin/postrecoveryboot.sh
 
 # BT stuff
